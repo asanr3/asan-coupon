f421e0e4de42ac398248708c89d5ebcd78ac4f58
yunmingkai <asanr3@163.com>
2021-06-11 17:35:21 +0800

    添加模板服务

---
+++ asan-coupon-service/coupon-template/src/main/java/com/asan/coupon/TemplateApplication.java
@@ -0,0 +1,24 @@
+package com.asan.coupon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+/**
+ * @author Asan
+ * @date 2021/6/11
+ * 模板微服务的启动入口
+ * @EnableScheduling 定时任务注解
+ * @EnableJpaAuditing 审计功能注解
+ * @EnableEurekaClient 服务发现注解
+ */
+@EnableScheduling
+@EnableEurekaClient
+@SpringBootApplication
+public class TemplateApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TemplateApplication.class,args);
+    }
+}
